abc49162c1845264c9f9001d4f6d0e06e49e6c8d
jkrouse13 <jon.krouse@gmail.com>
2016-10-11 13:29:39 -0400

    Added play again function.  Sometimes it doesnt understand p for paper

--- game.rb
+++ game.rb
@@ -1,4 +1,3 @@
-
 class Game
   attr_accessor :player_one_choice, :computer
 
@@ -10,8 +9,7 @@ class Game
     gets
   end
 
-  def play
-    new_game
+  def game_play
     puts "Make your selection!"
 
     self.player_one_choice = gets.chomp.downcase
@@ -19,34 +17,54 @@ class Game
 
     if player_one_choice == "r" && computer == "paper"
       puts "Player: ROCK vs Computer: PAPER ----- COMPUTER WINS"
+      play_again?
     elsif player_one_choice == "r" && computer == "scissors"
       puts "Player: ROCK vs Computer: SCISSORS ----- PLAYER WINS"
+        play_again?
     elsif player_one_choice == "r" && computer == "rock"
       puts "Player: ROCK vs Computer: ROCK ----- DRAW"
+        play_again?
     elsif player_one_choice == "s" && computer == "scissors"
       puts "Player: SCISSORS vs Computer: SCISSORS ----- DRAW"
+        play_again?
     elsif player_one_choice == "s" && computer == "paper"
       puts "Player: SCISSORS vs Computer: PAPER ----- PLAYER WINS"
+        play_again?
     elsif player_one_choice == "s" && computer == "ROCK"
       puts "Player: SCISSORS vs Computer: ROCK ----- COMPUTER WINS"
+        play_again?
     elsif player_one_choice == "p" && computer == "scissors"
       puts "Player: PAPER vs Computer: SCISSORS ----- COMPUTER WINS"
+        play_again?
     elsif player_one_choice == "p" && computer == "rock"
       puts "Player: PAPER vs Computer: ROCK ----- PLAYER WINS"
+        play_again?
     elsif player_one_choice == "p" && computer == "scissors"
       puts "Player: PAPER vs Computer: PAPER ----- DRAW"
+        play_again?
     else
       puts "I'm sorry I didn't understand you.  Let's try again."
       play
     end
+  end
 
 
-
+  def play
+    new_game
+    game_play
 
 
   end
 
-
+  def play_again?
+    puts "Would you like to play again? (Press y to play again)"
+    again = gets.chomp.downcase
+    if again == "y"
+      game_play
+    else
+      exit
+    end
+  end
 
 end
 
